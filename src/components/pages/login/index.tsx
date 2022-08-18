f64be18e94438c7bc338f741ae52047df1c33304
Franco Marini <franco.marini@radiumrocket.com>
2022-08-18 11:21:13 -0300

    absolute path example and grouping

--- src/components/pages/login/index.tsx
+++ src/components/pages/login/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import { capitalizeFirstLetter } from 'src/utils/formatters';
+
 import styles from './login.module.css';
 
 const Login = () => {
   return (
     <section className={styles.container}>
-      <h2>Login</h2>
+      <h2>{capitalizeFirstLetter('login')}</h2>
     </section>
   );
 };
