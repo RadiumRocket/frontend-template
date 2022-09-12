2bd584b9edf0bbe6fd05c4d0776652841ef35f17
Franco Marini <franco.marini@radiumrocket.com>
2022-09-12 14:25:01 -0300

    setup github actions

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -5,7 +5,7 @@ import styles from './home.module.css';
 const Home = () => {
   return (
     <section className={styles.container}>
-      <h2>Home</h2>
+      <h2>Home screen s</h2>
     </section>
   );
 };
